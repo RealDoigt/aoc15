654ce45bb30f408c78fe130fd7ffed7c59d4eb5c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 08:22:57 -0500

    Update present.d

--- source/day2/present.d
+++ source/day2/present.d
@@ -10,4 +10,13 @@ struct Present
         width  = w;
         height = h;
     }
+    
+    auto smallestSide()
+    {
+        auto side1 = l * w, side2 = l * h, side3 = w * h;
+        
+        if (side1 < side2 && side1 < side3) return side1;
+        if (side2 < side3) return side2;
+        return side3;
+    }
 }
