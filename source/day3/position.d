fa83b70faaabce1a01295d83f7be5cc621da44ad
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 10:56:13 -0500

    Create position.d

---
+++ source/day3/position.d
@@ -0,0 +1,28 @@
+module day3.position;
+
+struct Position
+{
+    long x, y;
+    
+    bool opEquals(Position right) const
+    {
+        return x == right.x && y == right.y;
+    }
+    
+    void move(char direction)
+    {
+        final switch (arrow)
+        {
+            case '>': ++x; break;
+            case '<': --x; break;
+            case 'v': ++y; break;
+            case '^': --y; break;
+        }
+    }
+}
+
+auto canFind(ref Position[] positions, Position toFind)
+{
+    foreach(p; positions) if (p == toFind) return true;
+    return false;
+}
