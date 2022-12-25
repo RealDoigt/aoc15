9af05ff8cf3929a034ae77fbbbf84ffd8840c7fc
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 10:26:35 -0500

    Update day3_main.d

--- source/day3/day3_main.d
+++ source/day3/day3_main.d
@@ -1,6 +1,25 @@
 module day3.main;
 import common;
 
+struct Position
+{
+    long x, y;
+    
+    bool opEquals(Position right) const
+    {
+        return x == right.x && y == right.y;
+    }
+}
+
 void day3Main()
 {
+    santa = Position(0, 0);    
+    Position[] housesVisited;
+    
+    housesVisited ~= santa;
+    
+    foreach (line; "inputs/day2/input".readText.strip)
+    {
+        
+    }
 }
