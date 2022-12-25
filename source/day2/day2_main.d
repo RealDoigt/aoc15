1f31e00cf96f926f1c7f66ec3de70a44adf3cb08
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 08:52:00 -0500

    Update day2_main.d

--- source/day2/day2_main.d
+++ source/day2/day2_main.d
@@ -1,6 +1,21 @@
 module day2.main;
+import std.conv;
 import common;
 
 void day2Main()
 {
+    Present*[] presents;
+    
+    foreach (line;  "inputs/day1/input".readText.strip.split("\n"))
+    {
+        auto numberStrings = line.split("x");
+        presents ~= new Present
+        (
+            numberStrings[0].to!uint, 
+            numberStrings[1].to!uint,
+            numberStrings[2].to!uint
+        );
+    }
+    
+    "Total required paper will be %d feets!".writefln(presents.paperRequiredInFeet);
 }
