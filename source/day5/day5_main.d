60b20feaf3de43e6f66c19053c9eb80181ea7b4f
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 11:54:06 -0500

    Update day5_main.d

--- source/day5/day5_main.d
+++ source/day5/day5_main.d
@@ -1,5 +1,19 @@
 module day5.main;
+import common;
+
+auto containsVowels(string s)
+{
+    return s.canFind("a") || s.canFind("e") || s.canFind("i") || s.canFind("o") || s.canFind("u");
+}
+
+auto isNice(string s)
+{
+}
 
 void day5Main()
 {
+    ulong nice;
+    
+    foreach (line; "inputs/day5/input".readText.strip.split("\n"))
+        if
 }
