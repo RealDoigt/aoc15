43dd3e9fd39f8b6c585cfda1f8aa4d85330976e3
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 07:59:31 -0500

    Update day1_main.d

--- source/day1/day1_main.d
+++ source/day1/day1_main.d
@@ -9,9 +9,12 @@ void day1Main()
     foreach (i, parenthesis; "inputs/day1/input".readText.strip)
     {
         if (parenthesis == '(') ++floor;
-        else --floor;
         
-        if (!hasEnteredBasement && floor == -1) basement = i + 1;
+        else
+        {
+            --floor;
+            if (!hasEnteredBasement && floor == -1) basement = i + 1;
+        }
     }
     
     "Santa must go to floor %d. Santa first enters the basement at position %d.".writefln(floor, basement);
