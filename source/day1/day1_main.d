21826bf4eb55d365ea509d0fdd13eda57915ca3c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 07:49:38 -0500

    Update day1_main.d
    part 2

--- source/day1/day1_main.d
+++ source/day1/day1_main.d
@@ -3,13 +3,15 @@ import common;
 
 void day1Main()
 {
-    long floor;
+    long floor, basement;
     
-    foreach (parenthesis; "inputs/day1/input".readText.strip)
+    foreach (i, parenthesis; "inputs/day1/input".readText.strip)
     {
         if (parenthesis == '(') ++floor;
         else --floor;
+        
+        if (floor == -1) basement = i;
     }
     
-    "Santa must go to floor %d".writefln(floor);
+    "Santa must go to floor %d. Santa first enters the basement at position %d.".writefln(floor, basement);
 }
