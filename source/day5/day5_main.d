3bcf8d7e343b95797d7c574e2a91c31c5e0af7ce
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-25 11:58:27 -0500

    Update day5_main.d

--- source/day5/day5_main.d
+++ source/day5/day5_main.d
@@ -6,12 +6,17 @@ auto containsVowels(string s)
     return s.canFind("a") || s.canFind("e") || s.canFind("i") || s.canFind("o") || s.canFind("u");
 }
 
-auto containsLetterPair(string s)
+auto containsTwinPair(string s)
 {
     for(int i = 1; i < s.length; ++i)
         if (s[i] == s[i - 1]) return true;
     
     return false;
+}
+
+auto containsForbiddenPair(string s)
+{
+    return canFind("ab") || canFind("cd") || canFind("pq") || canFind("xy");
 }
 
 auto isNice(string s)
