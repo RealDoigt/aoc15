39fe7e6d3f01b90511099a58b34d6a572b73a4e4
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-26 10:42:31 -0500

    Update day5_main.d

--- source/day5/day5_main.d
+++ source/day5/day5_main.d
@@ -1,40 +1,6 @@
 module day5.main;
-import std.algorithm;
 import common;
-
-auto contains3Vowels(string s)
-{
-    int count;
-    
-    foreach(c; s)
-    {
-        switch (c)
-        {
-            case 'a','e','i','o','u': ++count; break;
-            default: break;
-        }
-    }
-    
-    return count >= 3;
-}
-
-auto containsTwinPair(string s)
-{
-    for(int i = 1; i < s.length; ++i)
-        if (s[i] == s[i - 1]) return true;
-    
-    return false;
-}
-
-auto containsForbiddenPair(string s)
-{
-    return s.canFind("ab") || s.canFind("cd") || s.canFind("pq") || s.canFind("xy");
-}
-
-auto isNice(string s)
-{
-    return s.contains3Vowels && s.containsTwinPair && !s.containsForbiddenPair;
-}
+import day5;
 
 void day5Main()
 {
