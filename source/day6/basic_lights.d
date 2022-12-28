6813a27e7dd1df875b3415f0a5ef7ca5c81c071f
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-28 13:40:41 -0500

    Create basic_lights.d

---
+++ source/day6/basic_lights.d
@@ -0,0 +1,32 @@
+module day6.stupid_lights;
+import day6.main;
+
+bool[size][size] lights;
+
+void setLight(size_t x, size_t y, string action)
+{
+    switch(action)
+    {
+        case "on":  lights[y][x] = true; break;
+        case "off": lights[y][x] = false; break;
+        default:    lights[y][x] = !lights[y][x];
+    }
+}
+
+void setLights(size_t startX, size_t startY, size_t endX, size_t endY, string action)
+{
+    for (size_t y = startY; y <= endY; ++y)
+        for (size_t x = startX; x <= endX; ++x)
+            setLight(x, y, action);
+}
+
+auto countLightsOn()
+{
+    ulong total;
+    
+    for (size_t y; y < size; ++y)
+        for (size_t x; x < size; ++x)
+            if (lights[y][x]) ++total;
+    
+    return total;
+}
