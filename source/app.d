68a1e1d4fa22cb1f7b25a428493abbf48b0527a4
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-28 12:14:26 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -2,16 +2,18 @@ import day1;
 import day2;
 import day3;
 import day5;
+import day6;
 import std.algorithm.searching;
 
 void main(string[] args)
 {
-    if (args.length == 1) day5Main;
+    if (args.length == 1) day6Main;
 
     {
         if (args.canFind("1")) day1Main;
         if (args.canFind("2")) day2Main;
         if (args.canFind("3")) day3Main;
         if (args.canFind("5")) day5Main;
+        if (args.canFind("6")) day5Main;
     }
 }
