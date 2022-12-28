b4b542f7cd37bd0ac436275d743ed06af3f34f98
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-28 13:55:24 -0500

    Update day6_main.d

--- source/day6/day6_main.d
+++ source/day6/day6_main.d
@@ -10,15 +10,33 @@ void day6Main()
     {
         auto values = line.split(" ");
         
-        setLights
-        (
+        auto stValues = 
+        [
             values[1].to!size_t, 
             values[2].to!size_t, 
             values[3].to!size_t, 
-            values[4].to!size_t, 
+            values[4].to!size_t
+        ];
+        
+        setLights
+        (
+            stValues[0], 
+            stValues[1], 
+            stValues[2], 
+            stValues[3], 
+            values[0]
+        );
+        
+        setAdjustableLights
+        (
+            stValues[0], 
+            stValues[1], 
+            stValues[2], 
+            stValues[3], 
             values[0]
         );
     }
     
     "There are %d lights on!".writefln(countLightsOn);
+    "The total brightness of the other lights is %d!".writefln(getTotalBrightness);
 }
