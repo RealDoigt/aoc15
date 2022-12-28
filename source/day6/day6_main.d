ad61d131e21206ece3d240c50e142a14c6d33501
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-28 12:29:58 -0500

    Update day6_main.d

--- source/day6/day6_main.d
+++ source/day6/day6_main.d
@@ -1,6 +1,18 @@
 module day6.main;
 import common;
 
+static bool[1000][1000] lights;
+
+void setLight(size_t x, size_t y, string action)
+{
+    switch(action)
+    {
+        case "on":  lights[y][x] = true; break;
+        case "off": lights[y][x] = false; break;
+        default:    lights[y][x] = !lights[y][x];
+    }
+}
+
 void day6Main()
 {
 }
